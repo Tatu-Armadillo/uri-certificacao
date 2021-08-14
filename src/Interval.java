a2adeec65cb24bbde64d2387112c9d7611d81841
Tatu-Armadillo <prohazor.fa@gmail.com>
2021-08-14 00:02:52 -0300

    Inicio do IF

---
+++ src/Interval.java
@@ -0,0 +1,12 @@
+package src;
+
+import java.util.Scanner;
+
+public class Interval {
+    
+    public static void main(String[] args) {
+        
+        Scanner read = new Scanner(System.in);
+        
+    }
+}
